441c1ec8fa5299d4fe721482dfbb19b86d3fe0d3
石毛秀悟 <ishigeshuugo@192.168.0.17>
2025-01-16 23:04:31 +0900

    最低限のcatを再実装する。

---
+++ sishige/microCat.rs
@@ -0,0 +1,52 @@
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+
+fn do_cat(path: String) {
+	println!("{}", &path);
+}
+
+fn main() {
+	let argv: Vec<String> = env::args().collect();
+	let argc = argv.len();
+
+	if argc < 2 {
+		return;
+	}
+
+	for i in 1..argc {
+		do_cat(argv[i].clone());
+	}
+}
+
+fn main() {
+    // Create a path to the desired file
+    // 目的ファイルに対する`Path`を作成
+    let path = Path::new("hello.txt");
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    // pathを読み込み専用モードで開く。これは`io::Result<File>`を返す。
+    let mut file = match File::open(&path) {
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
+        // `io::Error`の`description`メソッドはエラーを説明する文字列を返す。
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    // ファイルの中身を文字列に読み込む。`io::Result<usize>`を返す。
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
+
+    // `file` goes out of scope, and the "hello.txt" file gets closed
+    // `file`がスコープから抜け、"hello.txt"が閉じられる。
+}
+
